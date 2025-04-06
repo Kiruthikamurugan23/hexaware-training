4110d60a332e8f898460d30bfa595abb105ae845
Kiruthikamurugan23 <97123532+Kiruthikamurugan23@users.noreply.github.com>
2025-04-06 19:02:30 +0530

    Delete hexaware training/assignment_banking_system directory

--- hexaware training/assignment_banking_system/src/com/hexaware/exception/InsufficientFundException.java
+++
@@ -1,9 +0,0 @@
-package com.hexaware.exception;
-
-public class InsufficientFundException extends Exception {
-    private static final long serialVersionUID = 1L;
-
-    public InsufficientFundException(String message) {
-        super(message);
-    }
-}
--- hexaware training/assignment_banking_system/src/com/hexaware/exception/InvalidAccountException.java
+++
@@ -1,9 +0,0 @@
-package com.hexaware.exception;
-
-public class InvalidAccountException extends Exception {
-    private static final long serialVersionUID = 1L;
-
-    public InvalidAccountException(String message) {
-        super(message);
-    }
-}
